64bbe9d581322ab5710dd77042525efd75320ca7
Andres Court <andres@andrescourt.com>
2026-08-22 20:10:20 -0500

    feat(tanstack start): the root of your application

---
+++ src/routes/__root.tsx
@@ -0,0 +1,38 @@
+import type { ReactNode } from 'react'
+import {
+	createRootRoute,
+	HeadContent,
+	Scripts,
+} from '@tanstack/react-router'
+
+export const Route = createRootRoute({
+	head: () => ({
+		meta: [
+			{
+				charSet: 'utf-8',
+			},
+			{
+				name: 'viewport',
+				content: 'width=device-width, initial-scale=1',
+			},
+			{
+				title: 'TanStack Start Starter',
+			},
+		],
+	}),
+	shellComponent: RootDocument
+})
+
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+	return (
+		<html>
+			<head>
+				<HeadContent />
+			</head>
+			<body>
+				{children}
+				<Scripts />
+			</body>
+		</html>
+	)
+}
